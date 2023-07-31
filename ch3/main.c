8d32c24f17f54e6ace6edcfca5a5c9d13870f8dc
Ahmed Hossam <hoss4614@gmail.com>
2023-07-31 04:02:18 +0300

    argv[]

--- ch3/main.c
+++ ch3/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#define MAXLINES 5000
+char *lineptr[MAXLINES];
 
-int main() {
-	printf("test\n");
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(void *lineptr[], int left, int right, int (*comp)(void *, void *));
+
+int numcmp(char *, char*);
+
+int main(int argc, char *argv[]) {
+
+	for(int i = 0; i < argc; ++i) {
+		if (*argv[i] == *"fuck") printf("%s\n", "FUCK YOU TOOOO");
+		//printf("%s\n", argv[i]);
+	}
+
+	return 0;
 }
